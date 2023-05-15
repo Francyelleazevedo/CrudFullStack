b4bef3340e60896d5183f5832a0ae8f95d89b78d
Francyelleazevedo <106888970+Francyelleazevedo@users.noreply.github.com>
2023-05-15 16:45:45 -0300

    Add files via upload

---
+++ Scripts/Create_TBCliente.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Cliente] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(100) NOT NULL,
+    [Email] varchar(100) NOT NULL,
+    [Telefone] varchar(11) NOT NULL,
+    [Cidade] varchar(100) NOT NULL,
+    CONSTRAINT [PK_Cliente] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230504195208_ClienteMigration', N'7.0.5');
+GO
+
+COMMIT;
+GO
+
---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using Dominio.Interfaces;
+using Infra.Conexao;
+using Infra.Repositorio;
+using Api.Dto;
+using CadastroCliente.Classes;
+using CadastroCliente.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            var server = Configuration["database:mysql:server"];
+            var port = Configuration["database:mysql:port"];
+            var database = Configuration["database:mysql:database"];
+            var username = Configuration["database:mysql:username"];
+            var password = Configuration["database:mysql:password"];
+            services.AddControllers();
+            services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseSqlServer(@"data source=DESKTOP-F3DQKQC;initial catalog=DEV-FULLSTACK;TrustServerCertificate=True;Integrated Security=SSPI", opt =>
+                {
+                    opt.MigrationsAssembly("Api");
+                    opt.CommandTimeout(180);
+                    opt.EnableRetryOnFailure(5); //testar 5 vezes
+                });
+            });
+
+            services.AddScoped<IRepository<Cliente>, Repository<Cliente>>();
+            services.AddScoped<IService<Cliente>, BaseServiceCliente<Cliente>>();
+
+            services.AddSingleton(new MapperConfiguration(config =>
+            {
+                config.CreateMap<CreateClienteDto, Cliente>();
+                config.CreateMap<Cliente, ClienteDto>();
+                config.CreateMap<ClienteDto, Cliente>();
+            }).CreateMapper());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors(builder =>
+            builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
